65a9c381567633f2ba64a2dd6c124b116d3268e4
rim-web <128071051+rim-web@users.noreply.github.com>
2023-04-17 14:09:59 +0500

    Add files via upload

---
+++ leapYear.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.println("The year is a leap year");
+        } else {
+            System.out.println("The year is not a leap year");
+        }
+    }
+}
+
